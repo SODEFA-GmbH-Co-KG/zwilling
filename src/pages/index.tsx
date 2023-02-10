cd7a244fa84f25c45159ba9bea2192ba94d0ad9b
Tristan Rechenberger <rechenberger@me.com>
2023-02-10 12:00:27 +0100

    🎉

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
 export default function Page() {
-  return <>hiii</>
+  return (
+    <>
+      <div className="container mx-auto p-4">
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-4xl font-bold">Hello World</div>
+          <div className="text-2xl font-bold">Hello World</div>
+          <div className="text-3xl font-bold">Hello World</div>
+
+          <div className="h-20 w-64 bg-red-200">Red</div>
+        </div>
+      </div>
+    </>
+  )
 }
